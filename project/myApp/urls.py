6561309c3ee3f4eef5eb242acaa88c2e1dee14aa
admin <admin@163.com>
2018-08-08 14:31:03 +0800

    2018-08-08 14:30

---
+++ project/myApp/urls.py
@@ -0,0 +1,16 @@
+#！/usr/bin/env python
+#-*- coding:utf-8 -*-
+# Author:zhangcs
+from django.conf.urls import url,include
+from . import views
+
+urlpatterns = [
+	#路由至主页
+	url(r'^$', views.Index),
+	url(r'^(\d+)/$',views.detail),
+	#路由至grades
+	url(r'^grades/$',views.grades),
+	#路由至students
+	url(r'^students/$',views.students),
+	url(r'^grades/(\d+)/$',views.gradesStudents),
+]
---
+++ project/myApp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Grades,Students
+
+# Create your views here.
+def Index(request):
+    return HttpResponse("Hello Python!!!!")
+
+def detail(request,num):
+	return HttpResponse("detail-%s"%num)
+	
+def grades(request):
+	#从模型中获取数据
+	gradesList = Grades.objects.all()
+	#将数据返回至grades.html
+	return render(request,"myApp/grades.html",{"grades":gradesList}) 
+
+def students(request):
+	#从模型中获取数据
+	studentsList = Students.objects.all()
+	#将数据返回至grades.html
+	return render(request,"myApp/students.html",{"students":studentsList})
+
+def gradesStudents(request,num):
+	grade = Grades.objects.get(id=num)
+	studentsList = grade.students_set.all()
+
+	return render(request,'myApp/students.html',{'students':studentsList})
